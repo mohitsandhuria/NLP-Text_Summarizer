1a938d167269efb2f24ad7c6189dc212afaaba3f
mohitsandhuria <mohitsandhuria@gmail.com>
2024-02-21 18:19:21 +0530

    logging

--- src/text_summarizer/logging/__init__.py
+++ src/text_summarizer/logging/__init__.py
@@ -4,10 +4,9 @@ import logging
 
 logging_str= "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-logs_dir="logs"
-
-log_filepath=os.path.join(logs_dir,"running_logs.log")
-os.makedirs(log_filepath,exist_ok=True)
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
 
 
 logging.basicConfig(
